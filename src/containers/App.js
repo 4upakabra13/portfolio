e014d65c0513ae307e5fce72aeabfc83a0753c0e
x <uns2801@gmail.com>
2022-04-26 00:36:26 +0200

    Add the coolest nav bar in history of the Internet

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import vid_main from './vid_main.mp4';
+import {Icon} from '@iconify/react'
 
 function App() {
   return (
     <div className="App flex flex-column">
       <div className='first flex '>
-        <video autoPlay loop muted className='vid_main'>
+        <video autoPlay loop muted className='vid_main' id='vido'>
           <source src={vid_main} type="video/mp4"></source>
         </video>
         <img src='/images/my_photo.jpeg' alt='my photo'
@@ -15,7 +16,21 @@ function App() {
       <h1 className='text_first greet'> Hi I'm Nick</h1>
       <h2 className='text_first love'>I love technology and coding</h2>
       <div className="topnav">
-          a
+          <div className='namee'>
+            Nikita Utkin
+          </div>
+          <div className='skills_nav'>
+            <a className='butt_nav no-underline hover-hot-pink pointer '>About</a>
+            <a className='butt_nav no-underline hover-hot-pink pointer '>Home</a>
+            <a className='butt_nav no-underline hover-hot-pink pointer '>Skillset</a>
+            <a className='butt_nav no-underline hover-hot-pink pointer '>Projects</a>
+            <a className='butt_nav no-underline hover-hot-pink pointer '>Contact</a>
+          </div>
+          <div className='icons'>
+          <a href='https://github.com/4upakabra13' target="_blank"><Icon icon="akar-icons:github-fill" color='#00d5ea' className='github_icon icons_h grow dib fw6 db link'/></a>
+          <Icon icon="academicons:cv" color='#00d5ea' className='icons_h cv_icon grow dib fw6 db link'/>
+          <a href='https://www.linkedin.com/in/nikita-utkin-lmao1/ target="_blank"'><Icon icon="akar-icons:linkedin-box-fill" color='#00d5ea' className='icons_h linkedin_icon grow dib fw6 db link'/></a>
+          </div>
       </div>
       <div className='second'>
         
